e27fd5a0b61758ff2e807a759696f13be8f8f850
Aethersis <maksym.walczak@gmail.com>
2017-08-08 18:03:56 +0200

    Fix error messages

--- Gw2Midi/Gw2Midi.cpp
+++ Gw2Midi/Gw2Midi.cpp
@@ -15,13 +15,13 @@ int main()
 	catch (const std::exception& exception)
 	{
 		std::cout << "An error occured: " << exception.what() << std::endl;
-		std::cout << "Press any key to exit.";
+		std::cout << "Press enter to quit.";
 		std::cin.get();
 
 		return 1;
 	}
 
-	std::cout << "Everything seems ok. Pressing a key on your MIDI keyboard will generate key event for Guild Wars 2.\n Press q to quit.\n";
+	std::cout << "Everything seems ok. Pressing a key on your MIDI keyboard will generate key event for Guild Wars 2.\nPress q to quit or simply close the window.\n";
 
 	while (!_kbhit() && key != 'q')
 	{
